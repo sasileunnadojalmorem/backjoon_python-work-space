e7038a292a70e61f53d3e0dd490ce8ce6aac1268
안동대 백수 <79616817+sasileunnadojalmorem@users.noreply.github.com>
2021-04-16 13:50:23 +0900

    Update sedoku.py

--- sedoku.py
+++ sedoku.py
@@ -1,48 +1,59 @@
 def small(i):
-    c = (i/9-1) / 3
-    b = (i%9-1) / 3
+    c = (i//9)// 3
+    b = (i%9)// 3
     if c == b:
         if c+b == 0:
-            i = 1
+            ans = 0
+            return ans
         elif c+b==2:
-            i = 5
+            ans = 4
+            return ans
         elif c+b ==4:
-            i = 9
+            ans = 8
+            return ans
     elif c > b: 
         if c + b == 1:
-            i = 2
+            ans = 1
+            return ans
         elif c + b ==2:
-            i = 3
+            ans = 2
+            return ans
         elif c + b == 3:
-            i = 6
+            ans = 5
+            return ans
     elif c < b:
         if c + b == 1:
-            i = 4
+            ans = 3
+            return ans
         elif c + b ==2:
-            i = 7
+            ans = 6
+            return ans
         elif c + b == 3:
-            i = 8
-    return i 
+            ans = 7
+            return ans
+a = small(81)
+print(a)
 a =[]
-for i in range(0,9):
-    b = list(map(int,input().split()))  
-    for j in range(len(b)):
-        a.append(b[j])
 row = []
 ver = []
 smallsedoku = []
 q = []
 for j in range(0,9):
-    for i in range(0,9):
-        q.append(i+1)
-    row.extend(q)
-    ver.extend(q)
-    smallsedoku.extend(q)
-for i in range(len(a)):
+    q.append(j+1)
+for j in range(0,9):
+    row.extend([q])
+    ver.extend([q])
+    smallsedoku.extend([q])
+for i in range(0,9):
+    b = list(map(int,input().split()))  
+    for j in range(len(b)):
+        a.append(b[j])
+
+for i in range(0,len(a)):
     if a[i] != 0 :
-        row[i//9-1][a[i]-1] = 10
-        ver[i%9-1][a[i]-1] = 10
-        smallsedoku[small(i)][a[i]] = 10 
+        row[(i+1)//9][a[i]-1] = 10
+        ver[(1+i)%9-1][a[i]-1] = 10
+        smallsedoku[small(i+1)][a[i]-1] = 10 
 zerocount = []
 for i in range(len(a)):
     if a[i] == 0:
@@ -52,18 +63,19 @@ def sedouku(depth):
             if  depth == len(zerocount):
                 break
             for j in range(0,9):
-                if  j+1 == row[[zerocount[i]]//9-1][j] == ver[zerocount[i]%9-1][j] == smallsedoku[small[i]][j]:
+                if  j+1 == row[(1+[zerocount[i]])//9][j] and j+1 == ver[(1+zerocount[i])%9-1][j] and j+1 == smallsedoku[small(i+1)][j]:
                     continue
                     a[zerocount[i]] = j+1
-                    row[zerocount[i]//9-1[i]][j] = 10
-                    ver[zerocount[i]%9-1][j] = 10
-                    smallsedoku[small[i]][j] = 10
+                    row[(1+zerocount[i])//9][j] = 10
+                    ver[(1+zerocount[i])%9-1][j] = 10
+                    smallsedoku[small(i+1)][j] = 10
                     sedouku(depth+1)
-                    row[zerocount[i]//9-1[i]][j] = j+1
-                    ver[zerocount[i]%9-1][j] = j+1
-                    smallsedoku[small[i]][j] = j+1
+                    row[(1+zerocount[i])//9][j] = j+1
+                    ver[(1+zerocount[i])%9-1][j] = j+1
+                    smallsedoku[small(i+1)][j] = j+1
 b = sedoku(0)
 print(a)
+
 
 
 
